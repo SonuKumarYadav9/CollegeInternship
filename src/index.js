557a2fbd998697b9f844e9dc8b622091b0ee5e37
deepesh-192 <104340220+deepesh-192@users.noreply.github.com>
2022-06-28 17:59:55 +0530

    Added college schema

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// # PLEASE READ THE README FILE FIRST AND THIS IS NOT THE FINAL BRANCH, PLEASE DO TESTING ON FINAL BRANCH "project/blog"
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routes/route.js");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose
+  .connect("", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
---
+++ src/models/collegeModel.js
@@ -0,0 +1,23 @@
+const mongoose =require('mongoose')
+const collegeSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "name is required"],
+        unique:true
+    },
+    fullName:{
+        type:String,
+        required:[true, "fullName is required"]
+    },
+    logoLink:{
+        type:String,
+        required:[true, "logoLink is required"]
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    }
+},
+{timestamps:true})
+
+module.exports=mongoose.model('college',collegeSchema)
